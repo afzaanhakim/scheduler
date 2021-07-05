174584ec2f1f0493cdb8c5fb68bf245b98a167bb
Afzaan Hakim <afzaanhakim@Afzaans-MacBook-Pro.local>
2021-07-05 19:31:57 -0400

    implement daylistitems, styling and ran test to ensure the spots return message instead of just the number

---
+++ src/components/DayListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
+  const formatSpots = (props) => {
+    let val = "";
+    if (props.spots === 0) {
+      val += `no spots remaining`;
+    }
+    if (props.spots === 1) {
+      val += `${props.spots} spot remaining`;
+    }
+
+    if (props.spots > 1) {
+      val += `${props.spots} spots remaining`;
+    }
+    return val;
+  };
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{formatSpots(props)}</h3>
+    </li>
+  );
+}
