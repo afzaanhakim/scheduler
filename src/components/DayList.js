7486f148933141f862a211a95141a92b261e4963
Afzaan Hakim <afzaanhakim@Afzaans-MacBook-Pro.local>
2021-07-06 09:05:41 -0400

    pass key value for day as unique identifier for each day object

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,12 +2,12 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  console.log(props.days);
-
   const mapDay = props.days.map(day => {
+    console.log("this is dayid",day.id)
     return (
       <ul>
         <DayListItem
+          key ={day.id}
           name={day.name}
           spots={day.spots}
           selected={day.name === props.day}
