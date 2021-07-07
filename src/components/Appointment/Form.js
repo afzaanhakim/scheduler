b2fee9e0edba688b4c886f38806d365fa635a05d
afzaanhakim <afzaanhakim19@gmail.com>
2021-07-07 12:58:26 -0400

    modified onSave function on button click and added an onSubmit handler to prevent default submission on clicking enter

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,10 +6,18 @@ import Button from 'components/Button';
 export default function Form(props) {
 const [name, setName] = useState(props.name || "")
 const [interviewer, setInterviewer] = useState(props.interviewer || null);
+const reset =function() {
+  setName("");
+  setInterviewer(null);
+}
 
+const cancel = function () {
+  reset();
+  props.onCancel();
+}
   return (<main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
-    <form autoComplete="off">
+    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
       <input
         className="appointment__create-input text--semi-bold"
         type="text"
@@ -27,8 +35,8 @@ const [interviewer, setInterviewer] = useState(props.interviewer || null);
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
-      <Button danger onClick={props.onCancel}>Cancel</Button>
-      <Button confirm onClick= {props.onSave}>Save</Button>
+      <Button danger onClick={(event)=> cancel()}>Cancel</Button>
+      <Button confirm onClick= {(event) => props.onSave(name, interviewer)}>Save</Button>
     </section>
   </section>
 </main>);
