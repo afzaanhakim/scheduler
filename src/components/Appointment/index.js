906cbbf1edaa6681e2a8abb259f5a3a8ee8c7ddb
afzaanhakim <afzaanhakim19@gmail.com>
2021-07-08 13:15:00 -0400

    remove typo

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,7 +3,7 @@ import "components/Appointment/styles.scss"
 import Header from './Header';
 import Empty from './Empty';
 import Show from './Show';
-import { tsPropertySignature } from '@babel/types';
+
 
 
 export default function Appointment (props){
