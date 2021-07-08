75407637b790a5844a0b40205ac2c08f1595f43b
afzaanhakim <afzaanhakim19@gmail.com>
2021-07-08 18:17:13 -0400

    add unit tests and built custom hooks to manage visual modes

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(nm, replace = false) {
+    setMode(nm);
+    const newHist = [...history]
+    if (replace) {
+      newHist.splice(-1, 1, nm);
+      return newHist;
+    } 
+      setHistory([...history, nm]);
+    
+  }
+
+  function back() {
+    if (history.length > 1) {
+      const newHist = [...history]
+      setMode(newHist[newHist.length - 2]);
+      setHistory(newHist.slice(0, -1));
+    }
+  }
+  return { mode, transition, back };
+}
