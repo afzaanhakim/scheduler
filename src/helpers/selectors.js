a9c3d6fe639a6fac1315dec388c6cf0292df1e93
afzaanhakim <afzaanhakim19@gmail.com>
2021-07-08 14:38:14 -0400

    add tests for the getinterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,21 @@
 export function getAppointmentsForDay(state, day) {
- const dayName = state.days.find(d =>  d.name === day )
- 
-if (!dayName){
-  return [];
+  const dayName = state.days.find((d) => d.name === day);
+
+  if (!dayName) {
+    return [];
+  }
+
+  const apps = dayName.appointments.map((id) => state.appointments[id]);
+  return apps;
 }
 
-const apps = dayName.appointments.map(id => state.appointments[id])
-return apps;
+export function getInterview(state, interview) {
+  const int = {};
+  if (!interview) {
+    return null;
+  } else {
+    int["student"] = interview.student;
+    int["interviewer"] = state.interviewers[interview.interviewer];
+  }
+  return int;
 }
