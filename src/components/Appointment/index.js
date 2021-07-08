a047891e9a0606cebcd320cf19fccdf25a2db263
afzaanhakim <afzaanhakim19@gmail.com>
2021-07-08 18:46:44 -0400

    use previously created custom hooks to trigger transitions and show form and go back to earlier state when clicked cancel

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import "components/Appointment/styles.scss"
-import Header from './Header';
-import Empty from './Empty';
-import Show from './Show';
+import React from "react";
+import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
+export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
-export default function Appointment (props){
-  return (<article className="appointment"><Header time = {props.time}/>{props.interview ? <Show student = {props.interview.student} interviewer  = {props.interview.interviewer}/> : <Empty/>}</article>)
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+
+      {mode === CREATE && (
+        <Form interviewers={[]} onCancel={() => back(EMPTY)} />
+      )}
+    </article>
+  );
 }
