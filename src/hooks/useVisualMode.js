fa8371a6702418f038ea3204027ee20e9a043119
afzaanhakim <afzaanhakim19@gmail.com>
2021-07-14 12:41:11 -0400

    re-implemented earlier version of setting history

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,18 +1,27 @@
+
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   function transition(nm, replace = false) {
+    setMode(nm);
+    const newHist = [...history]
     if (replace) {
-      setHistory((history) => [nm, ...history.slice(1)]);
-    } else {
-      setHistory((history) => [nm, ...history]);
-    }
+      newHist.splice(-1, 1, nm);
+      return newHist;
+    } 
+      setHistory([...newHist, nm]);
+    
   }
 
   function back() {
-    setHistory((history) => (history.length > 1 ? history.slice(1) : history));
+    if (history.length > 1) {
+      const newHist = [...history]
+      setMode(newHist[newHist.length - 2]);
+      setHistory(newHist.slice(0, -1));
+    }
   }
-  return { mode: history[0], transition, back };
+  return { mode, transition, back };
 }
